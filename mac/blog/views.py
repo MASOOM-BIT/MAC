d20a7fe2ad73f295f8a69fd7571663260ae409b2
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-11 00:21:48 +0530

    update blog index view to render template instead of plain text

--- mac/blog/views.py
+++ mac/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Blog Home')
+    return render(request,'blog/index.html')
 
 
 
