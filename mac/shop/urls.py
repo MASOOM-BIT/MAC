92843986b85ebc864f5cb5053350d33074de5bee
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-11 00:22:53 +0530

    refactor shop URLs to streamline path definitions

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -1,14 +1,6 @@
-
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="ShopHome"),
-    # path("about/", views.about, name="AboutUs"),
-    # path("contact/", views.contact, name="ContactUs"),
-    # path("tracker/", views.tracker, name="TrackingStatus"),
-    # path("search/", views.search, name="Search"),
-    # path("productview/", views.productView, name="ProductView"),
-    # path("checkout/", views.checkout, name="Checkout"),
+    path('', views.index ,name='shophome'),
 ]
-
