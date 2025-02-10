f7412d85276d15a7c37302f4d4d5780d5035e86e
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-11 00:22:31 +0530

    add homepage URL path to routing

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index ,name='HomePage'),
     path('shop/', include('shop.urls')),
-    path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls'))
 ]
 
