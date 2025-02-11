ec335027260b4d16c14c5734b88530bef17adb3e
Masoom Raza Ansari <masoomansari0786@gmail.com>
2025-02-12 01:07:56 +0530

    add: update URL patterns to include additional routes for about, contact, tracker, search, product view, and checkout

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -2,5 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index ,name='shophome'),
+    path('', views.index ,name='shopHome'),
+    path('about/', views.about,name="AboutUs"),
+    path('contact/', views.contact,name="ContactUs"),
+    path('tracker', views.tracker,name="TrackingStatus"),
+    path('search/', views.search,name="Search"),
+    path('productview/', views.productView,name="TrackingStatus"),
+    path('checkout/', views.checkout,name="Checkout"),
 ]
